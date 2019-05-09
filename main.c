0721dc4213e3804782fdc8c5d16bbf8fc1f7474b
Bastien Piguet <golden0kamia@gmail.com>
2019-05-09 16:13:17 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <math.h>
 
 void main(){
-  int b = 180-2*asin(sqrt(x^2+y^2)/2/5);
-  int a = 180-acos(x/sqrt(x^2+y^2))-b/2;
+  int b = -2*asin(sqrt(x^2+y^2)/10)+180;
+  int a = -acos(x*sqrt(x^2+y^2)/x^2+y^2)+asin(sqrt(x^2+y^2)/10)+90;
 }
