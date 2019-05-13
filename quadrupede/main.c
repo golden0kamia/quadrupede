bfac32610263c813d4d6c67048ed2c7560da15a8
Zérock <golden0kamia@gmail.com>
2019-05-13 22:15:51 +0200

    update

--- quadrupede/main.c
+++ quadrupede/main.c
@@ -2,8 +2,8 @@
  * quadrupede.c
  *
  * Created: 29.04.2019 21:20:19
- * Author : Bastien
- */ 
+ * Author : Bastien Piguet
+ */
 
 #define F_CPU 8000000UL
 
@@ -15,16 +15,17 @@ void delay_us();
 void servo(char angl, char pin);
 char alpha(int x, int y);
 char beta(int x, int y);
+void senduart(char txt);
+
+int baud = 9600;
+char txt;
 
 int main(void)
 {
-	UCSRA = 0;
-	UCSRB = (1<<TXEN)|(1<<RXEN);
-	UCSRC = (3<<UCSZ0);
-	UBRRL = 0b00110000;//0b
-	UBRRH = 0;
-	DDRB = 255;
-	DDRD |= (1<<PD1);
+	UCSR0B = (1<<TXEN0)|(1<<RXEN0);
+	UCSR0C = (3<<UCSZ00);
+	UBRR0L = F_CPU/(16*baud)+1;
+	DDRD |= (1<<PIND1);
 	
 	/*char posBase[1][4][2] = {{{0, 7}, {0, 7}, {0, 7}, {0, 7}}};
 	char walk[4][4][2] = {{{7, 7}, {0, 7}, {0, 7}, {7, 7}},
@@ -32,28 +33,46 @@ int main(void)
 						  {{0, 7}, {7, 7}, {7, 7}, {0, 7}},
 						  {{-7, 7}, {0, 7}, {0, 7}, {-7, 7}}};
 	char pin[4][2] = {{PINB0, PINB1}, {PINB2, PINB3}, {PINB4, PINB5}, {PINB6, PINB7}};
-	int speed;
-	char cmd = '1';*/
+	int speed;*/
+	char cmd;
 	
     while (1) 
     {
-		while(!(UCSRA & (1<<UDRE)));
-		UDR = 76;
-		while(!(UCSRA & (1<<UDRE)));
-		UDR = 79;
-		while(!(UCSRA & (1<<UDRE)));
-		UDR = 76;
-		while(!(UCSRA & (1<<UDRE)));
-		UDR = 10;
-		while(!(UCSRA & (1<<UDRE)));
-		_delay_ms(1000);
-		/*switch(UDR)
+		/*while(!(UCSR0A & (1<<UDRE0))){}
+		UDR0 = 'L';
+		while(!(UCSR0A & (1<<UDRE0))){}
+		UDR0 = 'O';
+		while(!(UCSR0A & (1<<UDRE0))){}
+		UDR0 = 'L';
+		while(!(UCSR0A & (1<<UDRE0))){}
+		UDR0 = '\n';
+		_delay_ms(1000);*/
+		if((UCSR0A & (1<<UDRE0))){
+			cmd = UDR0;
+		}
+		switch(cmd)
 		{
+		case '0' :
+			senduart('V');
+			break;
 		case '1' :
-			cmd = '1';
+			senduart('U');
+			break;
+		case '2' :
+			senduart('D');
+			break;
+		case '3' :
+			senduart('L');
+			break;
+		case '4' :
+			senduart('R');
+			break;
+		default:
 			break;
 		}
-		if(cmd == '1'){
+		cmd = 0;
+		_delay_ms(1000);
+		/*if(cmd == '1'){
 			for(char i; i<sizeof(walk); i++){
 				for(char j; j<sizeof(walk[i]); j++){
 					servo(alpha(walk[i][j][0], walk[i][j][1]), pin[j][0]);
@@ -81,9 +100,14 @@ void delay_us(int n){
 }
 
 char alpha(int x, int y){
-	//return -acos(x*sqrt(x*x+y*y)/x*x+y*y)*2*M_PI+asin(sqrt(x*x+y*y)/10)*2*M_PI+90;
+	return -acos(x*sqrt(x*x+y*y)/x*x+y*y)*2*M_PI+asin(sqrt(x*x+y*y)/10)*2*M_PI+90;
 }
 
 char beta(int x, int y){
-	//return -2*asin(sqrt(x*x+y*y)/10)*2*M_PI+180;
+	return -2*asin(sqrt(x*x+y*y)/10)*2*M_PI+180;
+}
+
+void senduart(char txt){
+	while(!(UCSR0A & (1<<UDRE0))){}
+	UDR0 = txt;
 }
